daef653a608787bc104d14b60a4aed60cb282ebe
Vishal14081997 <1408vishalsingh@gmail.com>
2026-08-05 19:17:05 +0530

    signup api

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import WelcomePanel from '../components/WelcomePanel'
 import { Link } from 'react-router-dom'
+import axios from "axios"
 
 const Signup = () => {
+    const [formData, SetFormData] = useState({
+        full_name: "",
+        email: "",
+        phone_no: "",
+        password: "",
+        userType: ""
+    })
+
+    // const handleChange = (e) => {
+    //     SetFormData((prev) => ({
+    //         ...prev,
+    //         [e.target.name]: e.target.value,
+    //     }));
+    // };
+
+    const handleChange = (e) => {
+        // console.log(e.target.name, e.target.value);
+        SetFormData({ ...formData, [e.target.name]: e.target.value })
+
+    }
+    console.log(formData);
+
+    const handleSignup = async (e) => {
+        e.preventDefault()
+        try {
+            const res = await axios.post("http://localhost:3000/auth/signup", formData)
+            console.log(res.data);
+        } catch (error) {
+            console.log(error.response?.data);
+        }
+    }
+
     return (
         <>
             <div className="flex bg-secondary">
@@ -24,38 +57,47 @@ const Signup = () => {
                             Join <span className="font-bold text-primary">ClickMart</span> and start shopping today.
                         </p>
 
-                        <form className="space-y-2">
+                        <form onSubmit={handleSignup} className="space-y-2">
                             <input
                                 type="text"
+                                name='full_name'
+                                value={formData.full_name}
+                                onChange={handleChange}
                                 placeholder="Full Name"
                                 className="w-full border border-gray-300 rounded-xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary"
                             />
 
                             <input
-                                type="text"
-                                placeholder="Username"
-                                className="w-full border border-gray-300 rounded-xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary"
-                            />
-
-                            <input
                                 type="email"
+                                name='email'
+                                value={formData.email}
+                                onChange={handleChange}
                                 placeholder="Email Address"
                                 className="w-full border border-gray-300 rounded-xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary"
                             />
 
                             <input
                                 type="tel"
+                                name="phone_no"
+                                value={formData.phone_no}
+                                onChange={handleChange}
                                 placeholder="Phone Number"
                                 className="w-full border border-gray-300 rounded-xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary"
                             />
 
                             <input
                                 type="password"
+                                name='password'
+                                value={formData.password}
+                                onChange={handleChange}
                                 placeholder="Password"
                                 className="w-full border border-gray-300 rounded-xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary"
                             />
 
                             <select
+                                name='userType'
+                                value={formData.userType}
+                                onChange={handleChange}
                                 className="w-full border border-gray-300 rounded-xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary"
                             >
                                 <option>Admin</option>
@@ -64,7 +106,7 @@ const Signup = () => {
                             </select>
 
                             <button
-                                type="submit"
+                                type='submit'
                                 className="w-full bg-primary hover:bg-orange-600 text-white font-semibold py-3 rounded-xl transition-all duration-300 shadow-lg hover:shadow-orange-300"
                             >
                                 Create Account
--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 import express from "express"
 import dbConnect from "./config/db.js"
 import authRoutes from "./routes/auth.route.js"
+import cors from "cors"
 
 import dotenv from "dotenv"
 dotenv.config()
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 app.use("/auth",authRoutes)
 
